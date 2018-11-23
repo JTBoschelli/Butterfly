3526322be4495e50c51e1f028c51f6200acfcfee
JustinB <justin.boschelli@wustl.edu>
2018-11-23 16:24:52 -0600

    Mapview complete with location services and annotations that can push to a detail view. location with test data is honolulu so relocate there for the example

---
+++ Butterfly/Event.swift
@@ -0,0 +1,43 @@
+//
+//  Event.swift
+//  Butterfly
+//
+//  Created by Justin Boschelli on 11/23/18.
+//  Copyright © 2018 Justin Boschelli. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Event: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+    init?(json: [Any]) {
+        // 1
+        self.title = json[16] as? String ?? "No Title"
+        self.locationName = json[12] as! String
+        self.discipline = json[15] as! String
+        // 2
+        if let latitude = Double(json[18] as! String),
+            let longitude = Double(json[19] as! String) {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = CLLocationCoordinate2D()
+        }
+    }
+}
---
+++ Butterfly/MapViewController.swift
@@ -0,0 +1,126 @@
+//
+//  MapViewController.swift
+//  Butterfly
+//
+//  Created by Justin Boschelli on 11/23/18.
+//  Copyright © 2018 Justin Boschelli. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var eventsArray: [Event] = []
+    var location: CLLocation? = nil
+    var locationManager:CLLocationManager!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        determineCurrentLocation()
+        
+    }
+    
+    
+    func determineCurrentLocation()
+    {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            //locationManager.startUpdatingHeading()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        mapView.showsUserLocation = true
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        //manager.stopUpdatingLocation()
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        loadInitialData()
+        mapView.addAnnotations(eventsArray)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func loadInitialData() {
+        // 1
+        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+            else { return }
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard
+            let data = optionalData,
+            // 2
+            let json = try? JSONSerialization.jsonObject(with: data),
+            // 3
+            let dictionary = json as? [String: Any],
+            // 4
+            let events = dictionary["data"] as? [[Any]]
+            else { return }
+        // 5
+        let validEvents = events.compactMap { Event(json: $0) }
+        eventsArray.append(contentsOf: validEvents)
+    }
+    
+
+}
+
+
+
+extension MapViewController: MKMapViewDelegate {
+    // 1
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 2
+        guard let annotation = annotation as? Event else { return nil }
+        // 3
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        // 4
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            // 5
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+}
