dc80e8c31af5bf01009bfd8cfc68da5fd9b7f974
Julia Dickerman <jcdickerman@wustl.edu>
2018-11-30 00:50:16 -0600

    Table View Setup

---
+++ Butterfly/EventsTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  EventsTableViewController.swift
+//  Butterfly
+//
+//  Created by Julia Dickerman on 11/30/18.
+//  Copyright © 2018 Justin Boschelli. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewController: UIViewController, UITableViewDataSource {
+    
+    var eventsArray: [Event] = []
+    @IBOutlet weak var eventsTable: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel!.text = eventsArray[indexPath.row].title
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eventsTable.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
